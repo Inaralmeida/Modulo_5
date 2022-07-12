27fed28937c678d8670a55935d4a4868d50b48b7
inaralmeida <inara.ralmeida@gmail.com>
2022-07-12 09:42:51 -0300

    add exercicios

---
+++ Revisao_Js/Exercicios/Aula2/exercicio1.js
@@ -0,0 +1,13 @@
+/*EXERCICIO 1 - Destructuring */
+/*Acessar todos os itens do array alunos
+Se atentar caso haja necessidade de renomear algo*/
+
+const alunos = [
+  "Alice",
+  "Gabriela",
+  "João",
+  "Francisco",
+  "Natália",
+  "Giovana",
+  "Gabriel",
+];

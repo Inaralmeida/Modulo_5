a1ed6cd513430fed2a458062f61f81488d2bb0d7
inaralmeida <inara.ralmeida@gmail.com>
2022-07-18 10:07:27 -0300

    add Conteudo de state

--- react-m5/src/Pages/Aula5/Aula5.jsx
+++ react-m5/src/Pages/Aula5/Aula5.jsx
@@ -1,12 +1,29 @@
 import React, { useState } from 'react'
-import BuscaPokemon from '../BuscaPokemon/BuscaPokemon'
+import Header from '../../Components/Header/Header'
 import '../../index.css'
 
 const Aula5 = () => {
   const url = 'https://randomuser.me/api/'
+  const [contador, setContador] = useState(0)
+
+  const [valorInput, setValorInput] = useState('')
+
+  function atualizaInput(event) {
+    console.log(event.target.value);
+    setValorInput(event.target.value)
+  }
+  function addMaisUm() {
+    setContador(contador + 1)
+  }
   return (
     <div className="container">
+      <Header titulo='Aula5' />
 
+      <input type="text" value={valorInput}
+        onChange={(event) => atualizaInput(event)}
+      />
+      <h1>{contador}</h1>
+      <button onClick={addMaisUm}>Adicionar</button>
     </div>
   )
 }

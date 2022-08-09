6baa0cb57bd62cd8aef2a0f26f46add0c9487116
inaralmeida <inara.ralmeida@gmail.com>
2022-08-09 19:30:04 -0300

    add atv buscando ususario

--- Revisao_Js/Conteudo/Aula2/Fetch.js
+++ Revisao_Js/Conteudo/Aula2/Fetch.js
@@ -1,19 +1,16 @@
-const buscarPokemon = (id) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json);
-    });
+// const buscarPokemon = (id) => {
+//   fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+//     .then((response) => response.json())
+//     .then((json) => {
+//       console.log(json);
+//     });
+
+// };
+
+const buscarPokemon = async (id) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const json = await response.json();
+  console.log(json);
 };
 
 buscarPokemon(25);
-
-// const buscarPokemon = async (id) => {
-//   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-//   const json = await response.json();
-//   console.log(json);
-//   const nome = json.name;
-//   const habilidades = json.abilities;
-//   console.log(nome);
-//   console.log(habilidades);
-// };
---
+++ Revisao_Js/Exercicios/Aula2/Buscando_Usuarios/script.js
@@ -0,0 +1,10 @@
+// url para requisicao
+const randomUserUrl = "https://randomuser.me/api/";
+
+async function gerarUsuario() {
+  //Seu código aqui
+}
+
+gerarUsuario();
+
+document.getElementById("changeUser").addEventListener("click", gerarUsuario);

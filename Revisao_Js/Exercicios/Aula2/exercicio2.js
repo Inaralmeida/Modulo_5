a70ce4ebc5f4dd70b1f5f9752f593920a4477f55
inaralmeida <inara.ralmeida@gmail.com>
2022-08-09 19:46:38 -0300

    add exercicios aula 2

---
+++ Revisao_Js/Exercicios/Aula2/exercicio2.js
@@ -0,0 +1,12 @@
+/* EXERCICIO 2  - Destructuring */
+
+// Acessar todos os itens do array, exceto o terceiro
+//Se atentar caso haja necessidade de renomear algo
+
+const disciplinas = [
+  "Matemática",
+  "Física",
+  "Química",
+  "Língua Portuguesa",
+  "Biologia",
+];
---
+++ Revisao_Js/Exercicios/Aula2/exercicio3.js
@@ -0,0 +1,28 @@
+/* EXERCICIO 3  - Destructuring */
+
+//Acessar todos os itens dos dois objetos disponíveis
+//Se atentar caso haja necessidade de renomear algo
+
+const dadosCliente = {
+  nome: "Fabio",
+  idade: 45,
+  endereco: {
+    logradouro: "Rua das Palmeiras",
+    numero: 42,
+    bairro: "Barra da Tijuca",
+    cidade: "Rio de Janeiro",
+  },
+  assinanteDesde: 2017,
+};
+
+const vestimenta = {
+  camisa: {
+    listrada: true,
+    cor: "Vermelha e branca",
+    colecao: "Outono/Inverno 2019",
+  },
+  calca: {
+    cor: "Azul escuro",
+    colecao: "Primavera/Verão 2021",
+  },
+};

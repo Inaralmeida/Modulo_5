938861801fd1bd796974b95bd61aadb9f9ff849c
inaralmeida <inara.ralmeida@gmail.com>
2022-08-09 20:20:45 -0300

    add conteudo destructuring

--- Revisao_Js/Conteudo/Aula2/Destructuring.js
+++ Revisao_Js/Conteudo/Aula2/Destructuring.js
@@ -2,16 +2,24 @@
 
 const frutas = ["Banana", "Uva"];
 
+const [fruta1, fruta2] = frutas;
+console.log("fruta1", fruta1);
+console.log("fruta2", fruta2);
+
 //ou
 
 const comidas = ["arroz", "Feijão", "alface", "Bife"];
-
+const [comida1, comida2, comida3, comida4] = comidas;
+console.log(comida1, comida4);
 //Exemplo  Objetos ---------------------------------------------------------
 
 const pessoa = {
   nome: "Irineu",
   sobrenome: "Você não sabe nem eu",
 };
+
+// const { nome, sobrenome } = pessoa;
+// console.log(nome, sobrenome);
 
 //Exemplo Objetos complexos---------------------------------------------
 
@@ -32,7 +40,14 @@ const compra = {
     sku: 2145786548,
   },
 };
-
+const {
+  comprador: {
+    nome,
+    endereco: { logradouro },
+  },
+  produto: { nome: nomeDoProduto },
+} = compra;
+console.log(nome, logradouro, nomeDoProduto);
 //Exemplo Objetos com array --------------------------------------------
 
 const resilia = {
@@ -44,3 +59,9 @@ const resilia = {
     { nome: "Cinthia", frente: "Tech" },
   ],
 };
+
+const {
+  empresa,
+  facilitadores: [, facilitadora2, , facilitadora3],
+} = resilia;
+console.log(facilitadora2, facilitadora3);
